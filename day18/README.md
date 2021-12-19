f80068039355e7fb2c510473fb30c20d8a66a571
Josh Heidecker <heidecjj@gmail.com>
2021-12-18 21:46:40 -0500

    day 18

---
+++ day18/README.md
@@ -0,0 +1,13 @@
+```commandline
+$ python3 part1.py < input.txt
+read_input: 1.9999861251562834e-06
+solve: 0.24211429999559186
+3869
+```
+
+```commandline
+$ python3 part2.py < input.txt
+read_input: 2.0999868866056204e-06
+solve: 6.347070899995742
+4671
+```
---
+++ day18/part1.py
@@ -0,0 +1,138 @@
+import sys
+from lib import timed_run
+from functools import reduce
+from dataclasses import dataclass
+
+
+@dataclass
+class Node:
+    left: object = None
+    right: object = None
+    value: int = None
+
+    @property
+    def is_leaf(self):
+        return self.value is not None
+
+    def __str__(self):
+        if self.is_leaf:
+            return str(self.value)
+        return f'[{self.left}, {self.right}]'
+
+
+@timed_run
+def read_input():
+    for line in sys.stdin:
+        yield eval(line.strip())
+
+
+def snail_add(left, right):
+    new = Node(left=left, right=right)
+    answer = snail_reduce(new)
+    return answer
+
+
+def snail_reduce(snail):
+    found = True
+    while found:
+        found = snail_explode(snail)
+        if found:
+            continue
+        found, snail = snail_split(snail)
+
+    return snail
+
+
+def snail_explode(snail):
+    nodes = list()
+
+    found = snail_explode_helper(snail, 0, nodes, False)
+
+    if found:
+        index = nodes.index(SENTINEL)
+
+        left_of_snail, exploded, right_of_snail = index - 1, index + 1, index + 2
+        exploded = nodes[exploded]
+        if left_of_snail > -1:
+            nodes[left_of_snail].value += exploded.left.value
+        if right_of_snail < len(nodes):
+            nodes[right_of_snail].value += exploded.right.value
+        exploded.left = exploded.right = None
+
+    return found
+
+
+SENTINEL = 'SENTINEL'
+
+
+def snail_explode_helper(snail, depth, nodes, found):
+    if snail.is_leaf:
+        nodes.append(snail)
+        return found
+
+    if not found and depth == 4:  # explode
+        snail.value = 0
+        nodes.append(SENTINEL)
+        nodes.append(snail)
+        found = True
+        return found
+
+    left = snail_explode_helper(snail.left, depth + 1, nodes, found)
+    found = found or left
+    right = snail_explode_helper(snail.right, depth + 1, nodes, found)
+    return found or right
+
+
+def depth_first_traverse(snail, nodes):
+    left, right = snail
+
+    if isinstance(left, int):
+        nodes.append((snail, 0))
+    else:
+        depth_first_traverse(left, nodes)
+
+    if isinstance(right, int):
+        nodes.append((snail, 1))
+    else:
+        depth_first_traverse(right, nodes)
+
+
+def snail_split(snail):
+    if snail.is_leaf:
+        if snail.value > 9:
+            half = snail.value // 2
+            return True, Node(left=Node(value=half), right=Node(value=snail.value - half))
+        return False, snail
+
+    found, snail.left = snail_split(snail.left)
+    if found:
+        return found, snail
+
+    found, snail.right = snail_split(snail.right)
+    return found, snail
+
+
+def snail_magnitude(snail):
+    if snail.is_leaf:
+        return snail.value
+    left, right = snail.left, snail.right
+    return 3 * snail_magnitude(left) + 2 * snail_magnitude(right)
+
+
+def treeify(snail):
+    if isinstance(snail, int):
+        return Node(value=snail)
+    return Node(left=treeify(snail[0]), right=treeify(snail[1]))
+
+
+@timed_run
+def solve(lines):
+    snails = list()
+    for snail in lines:
+        snails.append(treeify(snail))
+    answer = reduce(snail_add, snails)
+    return snail_magnitude(answer)
+
+
+if __name__ == '__main__':
+    print(solve(read_input()))
